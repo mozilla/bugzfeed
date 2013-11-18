54069df6a393322cc5fd3003e9416ea6db302445
Mark Côté <mcote@alumni.uwaterloo.ca>
2013-11-18 18:06:19 -0500

    Added license headers to example; fixed example html.

--- example/example.js
+++ example/example.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 var wsUri = 'ws://localhost:8844/';
 var bmoRestUrl = 'http://localhost:8080/bmo/rest';
 var websocket;
